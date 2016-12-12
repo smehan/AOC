e56d2a72b12aa7614a054fd090a5066e753235c9
Shawn Mehan <shawn.mehan@ieee.org>
2016-12-12 10:02:07 -0800

    part ii day 7. Includes some better output and control for both parts.

--- aoc_2016/d7.py
+++ aoc_2016/d7.py
@@ -6,6 +6,7 @@
 #  -*- coding: utf-8 -*-
 
 # standard libs
+import re
 
 # 3rd party libs
 import numpy as np
@@ -24,6 +25,7 @@ aba[bab]xyz supports SSL (aba outside square brackets with corresponding bab wit
 xyx[xyx]xyx does not support SSL (xyx, but no corresponding yxy).
 aaa[kek]eke supports SSL (eke in supernet with corresponding kek in hypernet; the aaa sequence is not related, because the interior character must be different).
 zazbz[bzb]cdb supports SSL (zaz has no corresponding aza, but zbz has a corresponding bzb, even though zaz and zbz overlap).
+pjxihfkytmmrowclw[savrpenougddqkuq]kfqivyjzfrpfwlftnq[tageosesgmlsmshmv]gjbgdqnwiwnltear[uoxbvzhexqonkbu]ivry
 """
 
 TEST_DATA = ['abba[mnop]qrst', 'abcd[bddb]xyyx', 'aaaa[qwer]tyui', 'ioxxoj[asdfgh]zxcvbn',
@@ -49,7 +51,6 @@ def is_abba(seq):
     :return:
     """
     if seq == seq[::-1] and seq[0] != seq[1]:
-        print(seq)
         return True
     else:
         return False
@@ -77,11 +78,65 @@ def parse_abba(s):
     return valid_abba
 
 
+def is_aba(s):
+    """
+    check sequence in s for XYX and returns that sequence if found, or None otherwise.
+    :param s:
+    :return:
+    """
+    if s[0] == s[2] and s[0] != s[1]:
+        return s
+    else:
+        return None
+
+
+def has_bab(seq, s):
+    """
+    Given an already found XYX in seq, find a reversed YXY in string s.
+    Return True if found, False otherwise.
+    :param seq:
+    :param s:
+    :return:
+    """
+    yxy = ''.join((seq[1], seq[0], seq[1]))
+    for match in re.finditer(r'\[[a-z]+\]', s):
+        if yxy in match.group(0):
+            return True
+    return None
+
+def parse_aba(s):
+    """
+    parses string s for ABA sequence and corresponding [...BAB...], True if both found,
+    None otherwise.
+    :param s:
+    :return:
+    """
+    valid_aba = False
+    bracket = False
+    full_s = s
+    while len(s) > 2:
+        next_seq = s[:3]
+        if '[' in next_seq:
+            bracket = True
+        if ']' in next_seq:
+            bracket = False
+        aba = is_aba(next_seq)
+        if aba and not bracket:
+            if has_bab(aba, full_s):
+                valid_aba = True
+                return valid_aba
+        s = s[1:]
+    return valid_aba
+
+
 if __name__ == '__main__':
     data = get_data()
-    count = 0
+    TLS_count = 0
+    SSL_count = 0
     for r in data:
         if parse_abba(r):
-            count += 1
-            print('valid IP7: {}'.format(r))
-    print('Total count = {}'.format(count))
+            TLS_count += 1
+        if parse_aba(r):
+            SSL_count += 1
+    print('Total TLS count = {}'.format(TLS_count))
+    print('Total SSL count = {}'.format(SSL_count))
