990828a23469106c84dff885a5e46b28f8127583
Shawn Mehan <shawn.mehan@ieee.org>
2016-12-12 15:42:31 -0800

    Better indexing and expansion. Correctly handles real data.

--- aoc_2016/d9.py
+++ aoc_2016/d9.py
@@ -6,6 +6,7 @@
 #  -*- coding: utf-8 -*-
 
 # standard libs
+import re
 
 # 3rd party libs
 import numpy as np
@@ -35,6 +36,68 @@ def get_data(test=False):
     else:
         return TEST_FILE
 
+
+def get_markers(s):
+    """
+    Pull out all valid markers in string, after stripping out any markers
+    invalidated by being in the shadow of a previous, valid marker.
+    :param s:
+    :return:
+    """
+    pattern = re.compile(r'\((\d+)x(\d+)\)')
+    markers = []
+    for m in re.finditer(pattern, s):
+        markers.append((int(m.group(1)), int(m.group(2)), m.span()[0], m.span()[1]))
+    for idx, tup in enumerate(markers):
+        if idx == 0:
+            out = [markers[idx]]
+        if idx > 0 and markers[idx][2] > (markers[idx-1][3] + markers[idx-1][0] - 1):
+            out.append(markers[idx])
+    return out
+
+
+def marker_starts(markers):
+    return (pos[2] for pos in markers)
+
+
+def expand_marker(idx, s, markers):
+    """
+
+    :param idx:
+    :param s:
+    :param markers:
+    :return: a new seq with expansion, next_pos to start copying again
+    """
+    for m in markers:
+        if idx == m[2]:
+            return s[m[3]:m[3] + m[0]] * m[1], m[3] + m[0]
+
+
+def decompress_stream(s):
+    """
+    Given string s, parse looking for valid compression markers.
+    Expand them and return a new, decompressed stream.
+    marker = (LengthxFreq)
+    :param s:
+    :return:
+    """
+    new_stream = ''
+    next_pos = 0
+    markers = get_markers(s)
+    for idx, char in enumerate(s):
+        if idx < next_pos:
+            continue
+        if idx in marker_starts(markers):
+            seq, next_pos = expand_marker(idx, s, markers)
+            new_stream += seq
+        else:
+            new_stream += char
+    print(s)
+    print(new_stream)
+    return new_stream
+
 if __name__ == '__main__':
-    get_data(test=True)
+    print('Decompressed stream has length {}'.format(len(decompress_stream(get_data()))))
+    # print('Test output: {}'.format(len(TEST_OUTPUT)))
+    # print(TEST_OUTPUT)
 
