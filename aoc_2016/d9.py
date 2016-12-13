fcce91ea44a4f52fc83ea7c3ff3958ec519c6fbb
Shawn Mehan <shawn.mehan@ieee.org>
2016-12-13 10:21:19 -0800

    Better doc and remove the deprecated functions.

--- aoc_2016/d9.py
+++ aoc_2016/d9.py
@@ -20,11 +20,15 @@ A(2x2)BCD(2x2)EFG doubles the BC and EF, becoming ABCBCDEFEFG for a decompressed
 (6x1)(1x3)A simply becomes (1x3)A - the (1x3) looks like a marker, but because it's within a data section of another marker, it is not treated any differently from the A that comes after it. It has a decompressed length of 6.
 X(8x2)(3x3)ABCY becomes X(3x3)ABC(3x3)ABCY (for a decompressed length of 18), because the decompressed data from the (8x2) marker (the (3x3)ABC) is skipped and not processed further."""
 
+"""
+    Version 2 multiplies all of the subsequent compressions markers, e.g. (8x2)(3x3) = (3x(2*3))
+    (27x12)(20x12)(13x14)(7x10)(1x12)A = A repeated 241920 times.
+    (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445 characters long
+"""
+
 TEST_FILE = """A(1x5)BC(3x3)XYZA(2x2)BCD(2x2)EFG(6x1)(1x3)AX(8x2)(3x3)ABCY"""
 
-#TEST_FILE_2 = """(27x12)(20x12)(13x14)(7x10)(1x12)A(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"""
-#TEST_FILE_2 = """(27x12)(20x12)(13x14)(7x10)(1x12)A"""
-TEST_FILE_2 = """(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"""
+TEST_FILE_2 = """(27x12)(20x12)(13x14)(7x10)(1x12)A(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"""
 
 TEST_OUTPUT = """ABBBBBCXYZXYZXYZABCBCDEFEFG(1x3)AX(3x3)ABC(3x3)ABCY"""
 
@@ -139,7 +143,7 @@ def divide_marker(s):
 
 def multiply(s):
     """
-    Given a string s, recursively calculate multipliers and lengths of regular chars
+    Given a string s, recursively calculate multipliers and lengths of regular chars.
     :param s: string of codes
     :return:
     """
@@ -159,39 +163,7 @@ def multiply(s):
         return front + multiply(seq) * marker[1] + multiply(remain)
 
 
-def multiply_markers(full_list):
-    """
-    Version 2 multiplies all of the subsequent compressions markers, e.g. (8x2)(3x3) = (3x(2*3))
-    (27x12)(20x12)(13x14)(7x10)(1x12)A = A repeated 241920 times.
-    (25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN becomes 445 characters long
-    :param full_list:
-    :return:
-    """
-    total = len(full_list)
-    out = []
-    for idx in range(total):
-        if total - idx > 0:
-            print(full_list[total-idx - 1])
-            if full_list[total - idx - 1][3] < full_list[total - idx - 2][2] + full_list[total - idx - 2][0]:
-                out.append((full_list[total - idx - 1][0],
-                            full_list[total - idx - 1][1] * full_list[total - idx - 2][1],
-                            full_list[total - idx - 1][2],
-                            full_list[total - idx - 1][3]))
-            else:
-                out.append(full_list[total - idx - 1])
-    print("Bang", out.reverse())
-        # if idx == 0:
-        #     out = [full_list[idx]]
-        # if idx > 0 and full_list[idx][3] < (full_list[idx-1][3] + full_list[idx-1][0] - 1):
-        #     print(full_list)
-
-
 if __name__ == '__main__':
-    #print('Decompressed stream has length {}'.format(len(decompress_stream(get_data(test=True)))))
-    #print('Test output: {}'.format(len(TEST_OUTPUT)))
-    #print(TEST_OUTPUT)
-    #print(multiply(init_stream(get_data(test=True, type=2))))
-    TEST_FILE_2 = '(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN'
-    print(multiply(init_stream(TEST_FILE_2)))
-    print('Test input was {}'.format(TEST_FILE_2))
+    print('Decompressed stream has length {}'.format(len(decompress_stream(get_data()))))
+    print('Version 2 stream has length {}'.format(multiply(init_stream(get_data()))))
 
