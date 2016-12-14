82565cd30fa1754294cbe634b6824f884554fa97
Shawn Mehan <shawn.mehan@ieee.org>
2016-12-14 13:46:29 -0800

    Clean it up. Better doc.

--- aoc_2016/d10.py
+++ aoc_2016/d10.py
@@ -7,7 +7,7 @@
 
 # standard libs
 from re import compile
-from operator import mul, itemgetter
+from operator import mul
 from functools import reduce
 from itertools import tee, filterfalse, chain
 
@@ -53,6 +53,12 @@ def get_data(test=False):
 
 
 def parse_lines(lines, regex):
+    """
+    return a list of captured groups in a tuple, filterd by regex match.
+    :param lines:
+    :param regex:
+    :return:
+    """
     return [regex.match(line).groups() for line in lines]
 
 
@@ -67,40 +73,69 @@ def partition(pred, iterable):
     return filterfalse(pred, t1), filter(pred, t2)
 
 
-def define_bot(bot, low, high, bins):
+def define_bot(low, high, bins):
+    """
+    Closure for setting state of bot
+    :param low:
+    :param high:
+    :param bins:
+    :return:
+    """
     def dist_a(a):
+
         def dist_b(b):
             h, l        = max(a, b), min(a, b)
             bins[high]  = bins[high](h)
             bins[low]   = bins[low](l)
             return h, l
+
         return dist_b
+
     return dist_a
 
 
 def bot_eval(inputs, cmds, bins):
+    """
+
+    :param inputs: list of chip inits, chip to a bot
+    :param cmds: list of bots giving chips to bots or bins
+    :param bins: {'bot_id': }
+    :return:
+    """
     for bot, low, high in cmds:
-        bins[bot] = define_bot(bot, low, high, bins)
+        bins[bot] = define_bot(low, high, bins)
 
     for val, bot in inputs:
         bins[bot] = bins[bot](int(val))
 
 
 def get_outputs(bins):
+    """
+    return a list of output bins.
+    :param bins:
+    :return:
+    """
     outputBins = ((k, v) for k, v in bins.items() if k.startswith("output"))
     return [v for k, v in sorted(outputBins, key=lambda x: int(x[0].split(" ")[-1]))]
 
 
-def day10(data):
+def process_bots(data, chip1, chip2):
+    """
+
+    :param data:
+    :param chip1: the first of two chips to search on
+    :param chip2: the second of two chips to search on
+    :return: tuple = (which both was responsible for eval of chip1 + chip2, chip values multipled from output bins 0..2)
+    """
     inputs, cmds = partition(lambda s: s.startswith("bot"), data)
     inputs, cmds = parse_lines(inputs, value_regex), parse_lines(cmds, botcmd_regex)
 
-    bins = {x:lambda y: y for x in chain.from_iterable(cmds)}
+    bins = {x: lambda y: y for x in chain.from_iterable(cmds)}
     bot_eval(inputs, cmds, bins)
 
     outputs = get_outputs(bins)
-    return {v:k for k, v in bins.items()}[(61, 17)], reduce(mul, outputs[:3], 1)
+    return {v: k for k, v in bins.items()}[(chip1, chip2)], reduce(mul, outputs[:3], 1)
 
 
 if __name__ == '__main__':
-    print(day10(get_data()))
+    print(process_bots(get_data(), chip1=61, chip2=17))
