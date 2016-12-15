c6634eb1aee907bbee609431057b6f777dc9e5cd
Shawn Mehan <shawn.mehan@ieee.org>
2016-12-15 13:40:11 -0800

    Initial stubbing for d12

---
+++ aoc_2016/d12.py
@@ -0,0 +1,14 @@
+###########################################################
+# Copyright (C) 2015-2016 Shawn Mehan <shawn dot mehan at shawnmehan dot com>
+# day 12 of AOC_2016
+###########################################################
+#
+#  -*- coding: utf-8 -*-
+
+# standard libs
+
+
+# 3rd party libs
+import numpy as np
+
+# application libs
