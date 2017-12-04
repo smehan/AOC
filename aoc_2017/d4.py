68400f19e32584a8e01aecd5d9d9e89af249e67c
Shawn Mehan <shawn.mehan@ieee.org>
2017-12-03 23:13:39 -0800

    Reduce checks.

--- aoc_2017/d4.py
+++ aoc_2017/d4.py
@@ -13,20 +13,22 @@ oiii ioii iioi iiio is not valid - any of these words can be rearranged to form 
 from collections import Counter
 
 
-def is_valid(phrase):
-    top = (Counter(phrase.strip('\n').split(' ')).most_common(1))
-    if top[0][1] > 1:
-        return False
-    else:
-        return True
-
-
-def no_anagram(phrase):
-    words = [''.join(sorted(w)) for w in phrase.strip('\n').split(' ')]
+def is_unique(words):
+    """pushes a list of words into a Counter and tests for no dupes in frequencies"""
     top = Counter(words).most_common(1)
     if top[0][1] > 1:
         return False
     return True
+
+
+def is_valid(phrase):
+    """tests for dupes in a split of words in phrase"""
+    return is_unique(phrase.strip('\n').split(' '))
+
+
+def no_anagram(phrase):
+    """tests for anagrams of words in phrase"""
+    return is_unique([''.join(sorted(w)) for w in phrase.strip('\n').split(' ')])
 
 
 def count_valid(ps):
@@ -46,6 +48,5 @@ if __name__ == '__main__':
             'a ab abc abd abf abj',
             'iiii oiii ooii oooi oooo',
             'oiii ioii iioi iiio']
-    print(count_valid(test))
     with open('d4.txt', 'r') as fh:
         print(count_valid(fh.readlines()))
