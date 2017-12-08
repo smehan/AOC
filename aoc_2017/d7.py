c7976c53e1f48c28553d675494a376d803387ee1
Shawn Mehan <shawn.mehan@ieee.org>
2017-12-08 00:19:50 -0800

    Day 7, part i

---
+++ aoc_2017/d7.py
@@ -0,0 +1,46 @@
+from copy import deepcopy
+
+test = """pbga (66)
+xhth (57)
+ebii (61)
+havc (66)
+ktlj (57)
+fwft (72) -> ktlj, cntj, xhth
+qoyq (66)
+padx (45) -> pbga, havc, qoyq
+tknk (41) -> ugml, padx, fwft
+jptl (61)
+ugml (68) -> gyxo, ebii, jptl
+gyxo (61)
+cntj (57)"""
+
+
+def parser(input: str):
+    parsed = []
+    for r in input.split('\n'):
+        tmp = []
+        for p in r.split(' '):
+            tmp.append(p)
+        parsed.append(tmp)
+    return parsed
+
+
+def sieve(tree: list):
+    trunk_elems = []
+    for node in tree:
+        # only those nodes that have map information can be anything but the outer leaves
+        if len(node) == 2:
+            continue
+        trunk_elems.append(node)
+    remaining = deepcopy(trunk_elems)
+    for node in trunk_elems:
+        for check in trunk_elems:
+            if node == check:
+                continue
+            others = ' '.join(check[3:])
+            if node[0] in others:
+                remaining.pop(remaining.index(node))
+    return remaining[0][0]
+
+
+print(sieve(parser(open('d7.txt').read())))
